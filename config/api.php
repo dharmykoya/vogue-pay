f2db84c69475a9194267d61eba2cf0278da0d264
Damilola Adekoya <dharmyoya38x@gmail.com>
2021-03-06 01:07:47 +0100

    feature(user-register):to allow users register

---
+++ config/api.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'env' => env("APP_ENV"),
+    'paystack' => [
+        'secret_key_test' => 'sk_test_788fc6ceb740105e8513f50e637412ef6c7da7b2',
+        'secret_key_live' => 'sk_test_788fc6ceb740105e8513f50e637412ef6c7da7b2'
+    ]
+];
